bc157dbdde0b1063da0f09f93dc4bb087d67c047
Basil Akavity <vakavity@gmail.com>
2024-03-20 21:13:20 +0300

    added HeaderPage, HeaderSteps

---
+++ src/main/java/org/akavity/pages/HeaderPage.java
@@ -0,0 +1,24 @@
+package org.akavity.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class HeaderPage {
+    private final SelenideElement accountButton = $(By.cssSelector("button[class*='userToolsToggler']"));
+
+    private final SelenideElement loginButton = $(By.cssSelector("button[data-testid='loginButton']"));
+
+    public SelenideElement getPromoItem(String title) {
+        return $(By.xpath("//div[contains(@class,'promo')]//li[contains(text(),'" + title + "')]"));
+    }
+
+    public SelenideElement getAccountButton() {
+        return accountButton;
+    }
+
+    public SelenideElement getLoginButton() {
+        return loginButton;
+    }
+}
---
+++ src/main/java/org/akavity/steps/HeaderSteps.java
@@ -0,0 +1,19 @@
+package org.akavity.steps;
+
+import org.akavity.pages.HeaderPage;
+
+public class HeaderSteps {
+    HeaderPage headerPage = new HeaderPage();
+
+    public void clickAccountButton() {
+        headerPage.getAccountButton().click();
+    }
+
+    public void clickLoginButton() {
+        headerPage.getLoginButton().click();
+    }
+
+    public void clickPromoItem(String title) {
+        headerPage.getPromoItem(title).click();
+    }
+}
