46626f7ba2e8a140a2feb39f448aa0299a3adb39
Basil Akavity <vakavity@gmail.com>
2024-03-26 21:42:47 +0300

    added JsonReader, annotation

---
+++ src/main/java/org/akavity/models/personalDataTest/NameGenderBirthData.java
@@ -0,0 +1,26 @@
+package org.akavity.models.personalDataTest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NameGenderBirthData {
+    String modalAttributName1;
+    String email;
+    String modalAttributName2;
+    String password;
+    String profileItem;
+    String personalData;
+    String modalAttributName3;
+    String name;
+    String gender;
+    String modalAttributName4;
+    String birth;
+    String actualName;
+    String actualGender;
+    String actualBirth;
+    String expectedGender;
+}
