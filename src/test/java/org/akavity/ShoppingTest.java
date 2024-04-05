f1a4a0a9c15eeeb7c65d9b47dc281170161389e1
Basil Akavity <vakavity@gmail.com>
2024-04-05 23:28:55 +0300

    added moveAroundTheCatalog test

---
+++ src/test/java/org/akavity/ShoppingTest.java
@@ -0,0 +1,25 @@
+package org.akavity;
+
+import org.akavity.annotations.TestData;
+import org.akavity.models.shoppingTest.CatalogData;
+import org.akavity.steps.ContentWrapperSteps;
+import org.akavity.steps.HeaderSteps;
+import org.akavity.steps.PopUpsSteps;
+import org.akavity.utils.JsonReader;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ShoppingTest extends BaseTest {
+    PopUpsSteps popUpsSteps = new PopUpsSteps();
+    HeaderSteps headerSteps = new HeaderSteps();
+    ContentWrapperSteps contentWrapperSteps = new ContentWrapperSteps();
+
+    @TestData(jsonFile = "catalogData", model = "CatalogData", folder = "shoppingTest")
+    @Test(description = "",
+            dataProviderClass = JsonReader.class, dataProvider = "getData")
+    public void moveAroundTheCatalog(CatalogData catalog) {
+        popUpsSteps.clickAcceptCookiesButton();
+        headerSteps.clickPromoItem(catalog.getPromoItem());
+        Assert.assertEquals(contentWrapperSteps.getTitle(), catalog.getTitle());
+    }
+}
