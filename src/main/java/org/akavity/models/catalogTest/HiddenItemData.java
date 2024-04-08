7f68c68c455764d7266ced465c283d903a4bab17
Basil Akavity <vakavity@gmail.com>
2024-04-08 23:26:37 +0300

    added test: selectHiddenCategoryItem, moveAroundTheCatalog, selectBrand

---
+++ src/main/java/org/akavity/models/catalogTest/HiddenItemData.java
@@ -0,0 +1,15 @@
+package org.akavity.models.catalogTest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HiddenItemData {
+    String chapterItem;
+    String allButton;
+    String categoryItem;
+    String expectedTitle;
+}
