cf4120e39685a42ff3caf5e008201229e1f69c55
Basil Akavity <vakavity@gmail.com>
2024-09-06 18:54:52 +0300

    changed locator, tests

--- src/test/java/org/akavity/ShoppingTest.java
+++ src/test/java/org/akavity/ShoppingTest.java
@@ -4,16 +4,18 @@ import org.akavity.annotations.TestData;
 import org.akavity.models.shoppingTest.*;
 import org.akavity.steps.*;
 import org.akavity.utils.JsonReader;
+import org.akavity.utils.Utils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class ShoppingTest extends BaseTest {
+public class ShoppingTest extends OldBaseTest {
     PopUpsSteps popUpsSteps = new PopUpsSteps();
     HeaderSteps headerSteps = new HeaderSteps();
     ContentWrapperSteps contentWrapperSteps = new ContentWrapperSteps();
     CartSteps cartSteps = new CartSteps();
     CatalogSteps catalogSteps = new CatalogSteps();
     FilterSteps filterSteps = new FilterSteps();
+    Utils utils = new Utils();
 
     @TestData(jsonFile = "catalogData", model = "CatalogData", folder = "shoppingTest")
     @Test(description = "Headers Navigation",
@@ -63,12 +65,9 @@ public class ShoppingTest extends BaseTest {
         catalogSteps.clickSubsectionItem(phone.getSubsectionItem());
         filterSteps.enterPrice(phone.getMinPrice(), phone.getMaxPrice());
         filterSteps.selectCheckbox(phone.getTitleManufacturer(), phone.getManufacturer());
-        filterSteps.selectCheckbox(phone.getTitleInternalMemory(), phone.getInternalMemory());
-        filterSteps.applyFiltersButton();
 
         Assert.assertTrue(contentWrapperSteps.areProductPricesWithinLimit(phone.getMinPrice(), phone.getMaxPrice()));
         Assert.assertTrue(contentWrapperSteps.doCatalogDescriptionsContainText(phone.getManufacturer()));
-        Assert.assertTrue(contentWrapperSteps.doCatalogDescriptionsContainText(phone.getInternalMemory()));
     }
 
     @TestData(jsonFile = "expensiveFilterData", model = "ExpensiveFilterData", folder = "shoppingTest")
